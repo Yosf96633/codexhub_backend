f5d06c482fce1f2ddd40c7c915508bb4fe49f114
Yosf96633 <yosf96633@gmail.com>
2025-07-20 20:51:57 +0500

    Add funtionality of rabbitMQ and worker

--- src/utils/watcher.ts
+++ src/utils/watcher.ts
@@ -2,16 +2,17 @@ import chokidar from "chokidar";
 import path from "path";
 import { sendToQueue } from "./mq";
 
-const outputDir = path.join(__dirname, "output");
+const outputDir = path.join(__dirname, "../../output");
 
 console.log("👀 Watching for new cloned repos in:", outputDir);
 
 const watcher = chokidar.watch(outputDir, {
-  ignoreInitial: true,
+  ignoreInitial: false,
   depth: 0,
 });
-
+console.log("After watcher")
 watcher.on("addDir", async (folderPath) => {
+  console.log("Function run")
   const folderName = path.basename(folderPath);
   console.log("Folder Path -> , " , folderPath);
     console.log("Folder Name -> , " , folderName);
---
+++ src/utils/worker.ts
@@ -0,0 +1,35 @@
+import amqp from 'amqplib';
+const QUEUE_NAME = 'code_folders';
+import {processRepo} from "../../../embedder/src/process"
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../../../embedder/.env') });
+console.log('🔑 OpenAI Key loaded:', process.env.OPENAI_API_KEY ? '✅ Yes' : '❌ No');
+
+async function startWorker() {
+  try {
+    const connection = await amqp.connect('amqp://localhost'); 
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(QUEUE_NAME, { durable: true });
+
+    console.log(`📡 Waiting for messages in queue: "${QUEUE_NAME}"`);
+
+    channel.consume(
+      QUEUE_NAME,
+      async msg => {
+        if (msg !== null) {
+          const messageContent = msg.content.toString();
+          console.log(`📥 Received message from queue: ${messageContent}`);
+          await processRepo(messageContent);
+          channel.ack(msg); 
+        }
+      },
+      { noAck: false }
+    );
+  } catch (err) {
+    console.error('❌ Worker failed:', err);
+  }
+}
+
+startWorker();
